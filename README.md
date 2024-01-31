e0e9b16eb6bf505792d87fe1567c2bf7e104dd83
carlostrujillo <krloslao90@gmail.com>
2024-01-30 20:33:45 -0500

    addin new logo and social + github

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# melocue-comingsoon
+
+# Melocue
+Discover the ultimate social network tailored for audio enthusiasts. Melocue brings you closer to the music you love through advanced audio tools, personalized AI recommendations, and a thriving marketplace. Enhance your audio experience with high-fidelity storage, exclusive content, and seamless smart home integration. Share your sound, learn from the best, and join our global community of melomaniacs.
+
+Melocue is comprise of several repos:
+
+Landing Page - [https://melocue.com/](https://melocue.com/)
+
+Frontend - [https://github.com/charleslaot/melocue-web](https://github.com/charleslaot/melocue-web)
+
+Backend - [https://github.com/charleslaot/melocue-api](https://github.com/charleslaot/melocue-api)
+
+Legacy (Code to integrate) - [https://github.com/charleslaot/melocue-legacy](https://github.com/charleslaot/melocue-legacy)
+
+## Demo
+
+Project is currently live, regular builds are pushed weekly, you can check the live app here:
+
+[https://app.melocue.com](https://app.melocue.com)
+
+
+## Contributing
+
+Contributions are always welcome!
+
+See `contributing.md` for ways to get started.
+
+Please adhere to this project's `code of conduct`.
+
+
+## Authors
+
+- [@charleslaot](https://www.github.com/charleslaot)
+
+
+## License
+
+[![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://github.com/charleslaot/melocue?tab=GPL-3.0-1-ov-file)
+
+
--- src/ComingSoon.jsx
+++ src/ComingSoon.jsx
@@ -52,8 +52,27 @@ const cards = [
 
 const navigation = [
   {
+    name: 'Github',
+    href: 'https://github.com/charleslaot/melocue',
+    icon: (props) => (
+      <svg
+        fill="currentColor"
+        viewBox="0 0 18 18"
+        className="h-6 w-6 relative top-0.5 left-1 text-lime-400"
+        {...props}
+      >
+        <path
+          fillRule="evenodd"
+          d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
+  },
+
+  {
     name: 'Facebook',
-    href: '#',
+    href: 'https://www.facebook.com/groups/1068028406986720',
     icon: (props) => (
       <svg
         fill="currentColor"
@@ -71,7 +90,7 @@ const navigation = [
   },
   {
     name: 'Instagram',
-    href: '#',
+    href: 'https://www.instagram.com/melocue_usa/',
     icon: (props) => (
       <svg
         fill="currentColor"
@@ -89,7 +108,7 @@ const navigation = [
   },
   {
     name: 'X',
-    href: '#',
+    href: 'https://twitter.com/melocue_usa',
     icon: (props) => (
       <svg
         fill="currentColor"
@@ -107,7 +126,7 @@ const navigation = [
   },
   {
     name: 'YouTube',
-    href: '#',
+    href: 'https://www.youtube.com/channel/UC-frvzWo6yHLnic6HZV-Mlw',
     icon: (props) => (
       <svg
         fill="currentColor"
@@ -192,6 +211,8 @@ export const ComingSoon = () => {
                 key={item.name}
                 href={item.href}
                 className="text-gray-400 hover:text-gray-500"
+                target="_blank"
+                rel="noreferrer"
               >
                 <span className="sr-only">{item.name}</span>
                 <item.icon aria-hidden="true" />
